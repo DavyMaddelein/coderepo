8ee80f9fee3a33630711472ec61beaa1981c43fe
Davy <Davy@157.193.160.250>
2012-12-06 08:52:03 +0100

    added FTP convenience methods class added some regex stuff added a tar/gzip and scp send class

--- src/main/java/com/compomics/coderepo/compressedstreams/CompressedInputStream.java
+++ src/main/java/com/compomics/coderepo/compressedstreams/CompressedInputStream.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package compressedstreams;
+package com.compomics.coderepo.compressedstreams;
 
 /**
  *
